66a75eee6138f53c5483a8ee677b12c5c24f76b4
unknown <pushpraj.p@honeysys.com>
2023-11-19 12:45:12 +0530

    login backend done

---
+++ fashion-hub/src/app/api/login/route.js
@@ -0,0 +1,78 @@
+import connectToDB from "@/database";
+import User from "@/models/user";
+import { compare } from "bcryptjs";
+import Joi from "joi";
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req) {
+  await connectToDB();
+
+  const { email, password } = await req.json();
+
+  const { error } = schema.validate({ email, password });
+
+  if (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.details[0].message,
+    });
+  }
+
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return NextResponse.json({
+        success: false,
+        message: "Account not found with this email",
+      });
+    } else {
+      const checkPassword = await compare(password, checkUser.password);
+      if (!checkPassword) {
+        return NextResponse.json({
+          success: false,
+          message: "Incorrect password, please try again !",
+        });
+      } else {
+        const token = jwt.sigh(
+          {
+            id: checkUser?._id,
+            email: checkUser?.email,
+            role: checkUser?.role,
+          },
+          "default_secret_key",
+          { expiresIn: "id" }
+        );
+
+        const finalData = {
+          token,
+          user: {
+            email: checkUser.email,
+            name: checkUser.name,
+            _id: checkUser._id,
+            role: checkUser.role,
+          },
+        };
+
+        return NextResponse.json({
+          success: true,
+          message: "Login successfull!",
+          finalData, //main user data after login
+        });
+      }
+    }
+  } catch (err) {
+    console.log("Error while logging In. Please try again");
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong ! Please try again later",
+    });
+  }
+}
