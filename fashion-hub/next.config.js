978cb14dec123d89004c1d913a0443db122d6637
unknown <pushpraj.p@honeysys.com>
2023-12-03 11:50:01 +0530

    cors configuration updated

--- fashion-hub/next.config.js
+++ fashion-hub/next.config.js
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/app/api/:path*',
-          headers: [
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            // Other headers...
-          ],
-        },
-      ];
-    },
-  };
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: [
+            "http://localhost:3000",
+            "https://fresh-fashion.vercel.app/"
+          ] },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
