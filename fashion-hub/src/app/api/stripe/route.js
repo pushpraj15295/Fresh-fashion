a33734cde6768f707ca9b64c106414f39bb27d31
unknown <pushpraj.p@honeysys.com>
2023-12-07 17:32:52 +0530

    get product by id url with checkout

--- fashion-hub/src/app/api/stripe/route.js
+++ fashion-hub/src/app/api/stripe/route.js
@@ -17,8 +17,8 @@ export async function POST(req) {
         payment_method_types: ["card"],
         line_items: res,
         mode: "payment",
-        success_url: "http://localhost:3000/checkout" + "?status=success",
-        cancel_url: "http://localhost:3000/checkout" + "?status=cancel",
+        success_url: "https://fresh-fashion.vercel.app/checkout" + "?status=success",
+        cancel_url: "https://fresh-fashion.vercel.app/checkout" + "?status=cancel",
       });
 
       return NextResponse.json({
