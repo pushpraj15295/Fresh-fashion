d6abb5202c4d363bcc24c7faa496b2f5bf3e970a
unknown <pushpraj.p@honeysys.com>
2023-12-07 15:02:55 +0530

    api changes

--- fashion-hub/src/services/cart/index.js
+++ fashion-hub/src/services/cart/index.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export const addToCart = async (formData) => {
   try {
-    const res = await fetch("http://localhost:3000/api/cart/add-to-cart", {
+    const res = await fetch("/api/cart/add-to-cart", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -38,7 +38,7 @@ export const getAllCartItems = async (id) => {
 
 export const deleteFromCart = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/cart/delete-from-cart?id=${id}`, {
+    const res = await fetch(`/api/cart/delete-from-cart?id=${id}`, {
       method: "DELETE",
       headers: {
         Authorization: `Bearer ${Cookies.get("token")}`,
